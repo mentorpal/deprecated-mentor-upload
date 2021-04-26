757aeda2c9ba0dadacff579bc77006fc063a7c86
larry kirschner <larrykirschner@gmail.com>
2021-04-26 16:32:24 -0700

    raises for no video upload

--- mentor_upload_worker/src/mentor_upload_process/process.py
+++ mentor_upload_worker/src/mentor_upload_process/process.py
@@ -1,8 +1,18 @@
+from os import environ, path
+
 from . import ProcessAnswerRequest, ProcessAnswerResponse
+
+
+def upload_path(p: str) -> str:
+    uploads_dir = path.abspath(environ.get("UPLOADS") or "uploads")
+    return path.join(uploads_dir, p)
 
 
 def process_answer_video(req: ProcessAnswerRequest) -> ProcessAnswerResponse:
     video_path = req.get("video_path", "")
     if not video_path:
         raise Exception("missing required param 'video_path'")
+    video_path_full = upload_path(video_path)
+    if not path.isfile(video_path_full):
+        raise Exception(f"video not found for path '{video_path}'")
     return req
--- mentor_upload_worker/tests/test_process_answer_video.py
+++ mentor_upload_worker/tests/test_process_answer_video.py
@@ -1,12 +1,23 @@
+from os import path
+import pytest
+
 from mentor_upload_process.process import process_answer_video
+from .utils import fixture_path, fixture_upload
 
 
-def test_transcribes_mentor_answer():
+@pytest.fixture()
+def uploads_fixture(monkeypatch) -> str:
+    uploads_path = fixture_upload("")
+    monkeypatch.setenv("UPLOADS", uploads_path)
+    return uploads_path
+
+
+def test_transcribes_mentor_answer(uploads_fixture):
     req = {"mentor": "m1", "question": "q1", "video_path": "video1.mp4"}
     assert process_answer_video(req) == req
 
 
-def test_raises_if_video_path_not_specified():
+def test_raises_if_video_path_not_specified(uploads_fixture):
     req = {"mentor": "m1", "question": "q1"}
     caught_exception = None
     try:
@@ -15,3 +26,14 @@ def test_raises_if_video_path_not_specified():
         caught_exception = err
     assert caught_exception is not None
     assert str(caught_exception) == "missing required param 'video_path'"
+
+
+def test_raises_if_video_not_found_for_path(uploads_fixture):
+    req = {"mentor": "m1", "question": "q1", "video_path": "not_exists.mp4"}
+    caught_exception = None
+    try:
+        process_answer_video(req)
+    except Exception as err:
+        caught_exception = err
+    assert caught_exception is not None
+    assert str(caught_exception) == "video not found for path 'not_exists.mp4'"
