ee08f33a34af85b56509b4fe7f379e5aad515120
Milan Simonovic <mbsimonovic@gmail.com>
2022-01-18 14:44:36 +0100

    remove unused

--- state-machine/api.py
+++ state-machine/api.py
@@ -52,15 +52,6 @@ class TaskInfo:
 
 
 @dataclass
-class UploadTaskRequest:
-    mentor: str
-    question: str
-    task_list: List[TaskInfo]
-    transcript: str = None
-    media: List[Media] = None
-
-
-@dataclass
 class UpdateTaskStatusRequest:
     mentor: str
     question: str
@@ -70,119 +61,8 @@ class UpdateTaskStatusRequest:
     media: Media = None
 
 
-@dataclass
-class MediaUpdateRequest:
-    mentor: str
-    question: str
-    media: Media
-
-
 class GQLQueryBody(TypedDict):
     query: str
-
-
-def answer_query_gql(mentor: str, question: str) -> GQLQueryBody:
-    return {
-        "query": """query Answer($mentor: ID!, $question: ID!) {
-            answer(mentor: $mentor, question: $question) {
-                _id
-                transcript
-                hasUntransferredMedia
-                media {
-                    type
-                    tag
-                    url
-                    needsTransfer
-                }
-            }
-        }""",
-        "variables": {
-            "mentor": mentor,
-            "question": question,
-        },
-    }
-
-
-def answer_upload_update_gql(req: AnswerUpdateRequest) -> GQLQueryBody:
-    variables = {}
-    variables["mentorId"] = req.mentor
-    variables["questionId"] = req.question
-    variables["answer"] = {"transcript": req.transcript, "media": req.media}
-    if req.has_edited_transcript is not None:
-        variables["answer"]["hasEditedTranscript"] = req.has_edited_transcript
-    return {
-        "query": """mutation UploadAnswer($mentorId: ID!, $questionId: ID!, $answer: UploadAnswerType!) {
-            api {
-                uploadAnswer(mentorId: $mentorId, questionId: $questionId, answer: $answer)
-            }
-        }""",
-        "variables": variables,
-    }
-
-
-def upload_task_req_gql(req: UploadTaskRequest) -> GQLQueryBody:
-    status = {}
-    status["taskList"] = req.task_list
-    if req.transcript:
-        status["transcript"] = req.transcript
-    if req.media:
-        status["media"] = req.media
-    return {
-        "query": """mutation UploadStatus($mentorId: ID!, $questionId: ID!, $status: UploadTaskInputType!) {
-            api {
-                uploadTaskUpdate(mentorId: $mentorId, questionId: $questionId, status: $status)
-            }
-        }""",
-        "variables": {
-            "mentorId": req.mentor,
-            "questionId": req.question,
-            "status": status,
-        },
-    }
-
-
-def fetch_question_name_gql(question_id: str) -> GQLQueryBody:
-    return {
-        "query": """query Question($id: ID!) {
-            question(id: $id){
-                name
-            }
-        }""",
-        "variables": {
-            "id": question_id,
-        },
-    }
-
-
-def fetch_answer(mentor: str, question: str) -> dict:
-    body = answer_query_gql(mentor, question)
-    res = requests.post(get_graphql_endpoint(), json=body)
-    res.raise_for_status()
-    tdjson = res.json()
-    if "errors" in tdjson:
-        raise Exception(json.dumps(tdjson.get("errors")))
-    data = tdjson["data"]["answer"]
-    return data
-
-
-def upload_update_answer(req: AnswerUpdateRequest) -> None:
-    headers = {"mentor-graphql-req": "true", "Authorization": f"bearer {get_api_key()}"}
-    body = answer_upload_update_gql(req)
-    res = requests.post(get_graphql_endpoint(), json=body, headers=headers)
-    res.raise_for_status()
-    tdjson = res.json()
-    if "errors" in tdjson:
-        raise Exception(json.dumps(tdjson.get("errors")))
-
-
-def upload_task_update(req: UploadTaskRequest) -> None:
-    headers = {"mentor-graphql-req": "true", "Authorization": f"bearer {get_api_key()}"}
-    body = upload_task_req_gql(req)
-    res = requests.post(get_graphql_endpoint(), json=body, headers=headers)
-    res.raise_for_status()
-    tdjson = res.json()
-    if "errors" in tdjson:
-        raise Exception(json.dumps(tdjson.get("errors")))
 
 
 def upload_task_status_req_gql(req: UpdateTaskStatusRequest) -> GQLQueryBody:
@@ -205,6 +85,30 @@ def upload_task_status_req_gql(req: UpdateTaskStatusRequest) -> GQLQueryBody:
     }
 
 
+def upload_answer_and_task_status_req_gql(
+    answer_req: AnswerUpdateRequest, status_req: UpdateTaskStatusRequest
+) -> GQLQueryBody:
+    variables = {}
+    variables["mentorId"] = answer_req.mentor
+    variables["questionId"] = answer_req.question
+    variables["answer"] = answer_req.new_status
+    variables["taskId"] = status_req.task_id
+    variables["newStatus"] = status_req.new_status
+    if status_req.transcript:
+        variables["transcript"] = status_req.transcript
+    if status_req.media:
+        variables["media"] = status_req.media
+    return {
+        "query": """mutation UpdateUploadAnswerAndTaskStatus($mentorId: ID!, $questionId: ID!, $taskId: String!, $newStatus: String!, $transcript: String, $media: [AnswerMediaInputType]) {
+            api {
+                uploadAnswer(mentorId: $mentorId, questionId: $questionId, answer: $answer)
+                uploadTaskStatusUpdate(mentorId: $mentorId, questionId: $questionId, taskId: $taskId, newStatus: $newStatus, transcript: $transcript, media: $media)
+            }
+        }""",
+        "variables": variables,
+    }
+
+
 def upload_answer_and_task_status_update(
     answerReq: AnswerUpdateRequest, statusReq: UpdateTaskStatusRequest
 ) -> None:
@@ -217,30 +121,6 @@ def upload_answer_and_task_status_update(
         raise Exception(json.dumps(tdjson.get("errors")))
 
 
-def upload_answer_and_task_status_req_gql(
-    answerReq: AnswerUpdateRequest, statusReq: UpdateTaskStatusRequest
-) -> GQLQueryBody:
-    variables = {}
-    variables["mentorId"] = answerReq.mentor
-    variables["questionId"] = answerReq.question
-    variables["answer"] = answerReq.new_status
-    variables["taskId"] = statusReq.task_id
-    variables["newStatus"] = statusReq.new_status
-    if statusReq.transcript:
-        variables["transcript"] = statusReq.transcript
-    if statusReq.media:
-        variables["media"] = statusReq.media
-    return {
-        "query": """mutation UpdateUploadAnswerAndTaskStatus($mentorId: ID!, $questionId: ID!, $taskId: String!, $newStatus: String!, $transcript: String, $media: [AnswerMediaInputType]) {
-            api {
-                uploadAnswer(mentorId: $mentorId, questionId: $questionId, answer: $answer)
-                uploadTaskStatusUpdate(mentorId: $mentorId, questionId: $questionId, taskId: $taskId, newStatus: $newStatus, transcript: $transcript, media: $media)
-            }
-        }""",
-        "variables": variables,
-    }
-
-
 def upload_task_status_update(req: UpdateTaskStatusRequest) -> None:
     headers = {"mentor-graphql-req": "true", "Authorization": f"bearer {get_api_key()}"}
     body = upload_task_status_req_gql(req)
@@ -249,92 +129,3 @@ def upload_task_status_update(req: UpdateTaskStatusRequest) -> None:
     tdjson = res.json()
     if "errors" in tdjson:
         raise Exception(json.dumps(tdjson.get("errors")))
-
-
-def fetch_question_name(question_id: str) -> str:
-    headers = {"mentor-graphql-req": "true", "Authorization": f"bearer {get_api_key()}"}
-    body = fetch_question_name_gql(question_id)
-    res = requests.post(get_graphql_endpoint(), json=body, headers=headers)
-    res.raise_for_status()
-    tdjson = res.json()
-    if "errors" in tdjson:
-        raise Exception(json.dumps(tdjson.get("errors")))
-    if (
-        "data" not in tdjson
-        or "question" not in tdjson["data"]
-        or "name" not in tdjson["data"]["question"]
-    ):
-        raise Exception(f"query: {body} did not return proper data format")
-    return tdjson["data"]["question"]["name"]
-
-
-def fetch_answer_transcript_and_media_gql(mentor: str, question: str) -> GQLQueryBody:
-    return {
-        "query": """query Answer($mentor: ID!, $question: ID!) {
-            answer(mentor: $mentor, question: $question){
-                hasEditedTranscript
-                transcript
-                media {
-                type
-                tag
-                url
-              }
-            }
-        }""",
-        "variables": {"mentor": mentor, "question": question},
-    }
-
-
-def fetch_answer_transcript_and_media(mentor: str, question: str):
-    headers = {"mentor-graphql-req": "true", "Authorization": f"bearer {get_api_key()}"}
-    body = fetch_answer_transcript_and_media_gql(mentor, question)
-    res = requests.post(get_graphql_endpoint(), json=body, headers=headers)
-
-    res.raise_for_status()
-    tdjson = res.json()
-    if "errors" in tdjson:
-        raise Exception(json.dumps(tdjson.get("errors")))
-    if (
-        "data" not in tdjson
-        or "answer" not in tdjson["data"]
-        or "media" not in tdjson["data"]["answer"]
-        or "transcript" not in tdjson["data"]["answer"]
-        or "hasEditedTranscript" not in tdjson["data"]["answer"]
-    ):
-        raise Exception(f"query: {body} did not return proper data format")
-    return (
-        tdjson["data"]["answer"]["transcript"],
-        tdjson["data"]["answer"]["media"],
-        tdjson["data"]["answer"]["hasEditedTranscript"],
-    )
-
-
-def media_update_gql(req: MediaUpdateRequest) -> GQLQueryBody:
-    return {
-        "query": """mutation MediaUpdate($mentorId: ID!, $questionId: ID!, $media: AnswerMediaInputType!) {
-            api {
-                mediaUpdate(mentorId: $mentorId, questionId: $questionId, media: $media)
-            }
-        }""",
-        "variables": {
-            "mentorId": req.mentor,
-            "questionId": req.question,
-            "media": req.media,
-        },
-    }
-
-
-def update_media(req: MediaUpdateRequest) -> None:
-    headers = {"mentor-graphql-req": "true", "Authorization": f"bearer {get_api_key()}"}
-    body = media_update_gql(req)
-    res = requests.post(get_graphql_endpoint(), json=body, headers=headers)
-    res.raise_for_status()
-    tdjson = res.json()
-    if "errors" in tdjson:
-        raise Exception(json.dumps(tdjson.get("errors")))
-
-
-def fetch_text_from_url(url: str) -> str:
-    res = requests.get(url)
-    res.raise_for_status()
-    return res.text
