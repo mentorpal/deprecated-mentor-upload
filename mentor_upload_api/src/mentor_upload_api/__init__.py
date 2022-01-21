f69654e037e94bea8168d688574a75d6ad994008
aaronshiel <ashiel408@gmail.com>
2022-01-21 15:34:10 -0800

    error handlers for http and validation errors, and also generic exception handler

--- mentor_upload_api/src/mentor_upload_api/__init__.py
+++ mentor_upload_api/src/mentor_upload_api/__init__.py
@@ -15,6 +15,7 @@ from logging.config import dictConfig  # NOQA E402
 from flask import Flask, request, g, has_request_context  # NOQA E402
 from flask_cors import CORS  # NOQA E402
 from werkzeug.exceptions import HTTPException  # NOQA E402
+from jsonschema import ValidationError  # NOQA E402
 from mentor_upload_api.blueprints.ping import ping_blueprint  # NOQA E402
 from mentor_upload_api.blueprints.upload.answer import answer_blueprint  # NOQA E402
 from mentor_upload_api.blueprints.upload.transfer import transfer_blueprint  # NOQA E402
@@ -151,11 +152,20 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    def error_handler(e):
-        # pass through HTTP errors
-        if isinstance(e, HTTPException):
-            return e
+    def generic_exception_handler(e):
+        """Return JSON instead of generic 500 internal error for Exceptions"""
+        response = app.response_class(
+            response=json.dumps({"error": "Exception", "message": str(e)}),
+            status=400,
+            content_type="application/json",
+        )
+        return response
 
+    app.register_error_handler(Exception, generic_exception_handler)
+
+    def http_error_handler(e):
+        """Return JSON instead of HTML for HTTP errors."""
+        # start with the correct headers and status code from the error
         response = e.get_response()
         # replace the body with JSON
         response.data = json.dumps(
@@ -168,7 +178,19 @@ def create_app():
         response.content_type = "application/json"
         return response
 
-    # app.register_error_handler(Exception, error_handler)
+    app.register_error_handler(HTTPException, http_error_handler)
+
+    def json_validation_error_handler(e):
+        response = app.response_class(
+            response=json.dumps(
+                {"error": "ValidationError", "message": str(e.message)}
+            ),
+            status=400,
+            content_type="application/json",
+        )
+        return response
+
+    app.register_error_handler(ValidationError, json_validation_error_handler)
 
     if os.environ.get("IS_SENTRY_ENABLED", "") == "true":
         logging.info("SENTRY enabled, calling init")
