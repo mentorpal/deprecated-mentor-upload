c610bf63aa9d09a03828dfeb034fa71912cd2e0f
aaronshiel <ashiel408@gmail.com>
2022-01-25 14:38:22 -0800

    raise BadRequest for missing payload params

--- mentor_upload_api/src/mentor_upload_api/helpers.py
+++ mentor_upload_api/src/mentor_upload_api/helpers.py
@@ -8,6 +8,7 @@ import json
 from functools import wraps
 from jsonschema import validate, ValidationError
 from flask import request
+from werkzeug.exceptions import BadRequest
 import requests
 import logging
 from os import environ
@@ -39,20 +40,20 @@ def validate_payload_json_decorator(json_schema):
     def validate_json_wrapper(f):
         @wraps(f)
         def json_validated_function(*args, **kwargs):
+            if not json_schema:
+                raise Exception("'json_schema' param not provided to validator")
+            body = request.form.get("body", {})
+            if body:
+                json_body = json.loads(body)
+            else:
+                json_body = request.json
+            if not json_body:
+                raise BadRequest("missing required param body")
             try:
-                if not json_schema:
-                    raise Exception("'json_schema' param not provided to validator")
-                body = request.form.get("body", {})
-                if body:
-                    json_body = json.loads(body)
-                else:
-                    json_body = request.json
-                if not json_body:
-                    raise Exception("missing required param body")
                 validate(instance=json_body, schema=json_schema)
                 return f(json_body, *args, **kwargs)
             except ValidationError as err:
-                logging.error(msg=err)
+                logging.error(err)
                 raise err
 
         return json_validated_function
