9355f7392225f23b87f4bd7e67293316c3ecf6f3
aaronshiel <ashiel408@gmail.com>
2022-04-27 19:58:53 -0700

    fixes

--- mentor_upload_worker/src/mentor_upload_process/process.py
+++ mentor_upload_worker/src/mentor_upload_process/process.py
@@ -896,9 +896,19 @@ def process_transfer_mentor(req: ProcessTransferMentor, task_id: str):
                         logging.error("Succesfully uploaded")
                         m["needsTransfer"] = False
                         m["url"] = item_path
+                        update_media_vars = {
+                            "mentor":mentor,
+                            "question":question
+                        }
+                        if tag == "en":
+                            update_media_vars["vtt_media"] = m
+                        if tag == "web":
+                            update_media_vars["web_media"] = m
+                        if tag == "mobile":
+                            update_media_vars["mobile_media"] = m
                         update_media(
                             MediaUpdateRequest(
-                                mentor=mentor, question=question, media=m
+                                **update_media_vars
                             )
                         )
                         answer_media_migrate_update = {
